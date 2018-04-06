8f4d402fe307f4ed1b51847fc126c2991b5ff94a
moses.adebayo <moses.adebayo@venturegardengroup.com>
2018-04-06 12:50:42 +0100

    Todo list populated on addition of tasks

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Button } from  'reactstrap';
 import './Home.css';
 import TodoPanel from "./todo-panel";
+import TodoList from "./todo-list";
 
 class Home extends Component {
     constructor (props){
@@ -17,16 +18,21 @@ class Home extends Component {
     processTaskSubmit = (event) => {
         event.preventDefault();
         const { currentTask, tasks } = this.state;
-        this.setState({tasks: [...tasks, currentTask ]});
-
+        this.setState({tasks: [currentTask, ...tasks ]});
+        this.resetForm();
+    };
+    resetForm = () => {
+      this.setState({currentTask: ''});
     };
     render () {
-        const { currentTask } = this.state;
+        const { currentTask, tasks } = this.state;
         return (
             <div className="container">
                 <section className="section">
                     <div className="container">
                         <TodoPanel taskEntered={currentTask} onTaskEntered={this.processTaskEntered} onTaskSubmit={this.processTaskSubmit}/>
+
+                        <TodoList tasks={tasks}/>
                     </div>
                 </section>
             </div>
